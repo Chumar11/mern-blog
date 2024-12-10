135b5d95e35537642a022730a7f4d84bb7189011
Chumar <chumarsarwar3@gmail.com>
2024-12-10 16:20:09 +0500

    user Model

--- api/index.js
+++ api/index.js
@@ -2,6 +2,12 @@ import express from "express";
 import db from "./db/dbconnect.js";
 const app = express();
 
+app.get('/',()=>{
+console.log("helo welcome");
+
+})
+
 app.listen(3001, () => {
   console.log("server is runing on port 3001");
 });
+ 
