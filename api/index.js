419c21022b8c6ec5adf4ac6c95e48d78ad1b3fc7
Chumar <chumarsarwar3@gmail.com>
2024-12-11 18:42:58 +0500

    add functionality to the sign up page

--- api/index.js
+++ api/index.js
@@ -5,19 +5,17 @@ import authRoutes from "./routes/auth.routes.js";
 const app = express();
 app.use(express.json());
 app.use("/", personRoutes);
-app.use("/", authRoutes);
+app.use("/api", authRoutes);
 
-app.use((err,req,res,next)=>{
-
-const statusCode=err.statusCode ||500;
-const message=err.message ||"Internal server error"
-res.status(statusCode).json({
-success:false,
-statusCode,
-message
-
-})
-})
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
 
 app.listen(3001, () => {
   console.log("server is runing on port 3001");
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,8 +1,46 @@
-import { Button, Label, TextInput } from "flowbite-react";
-import React from "react";
-import { Link } from "react-router-dom";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignUp() {
+  const [formData, setformData] = useState({});
+  const [errorMessage, seterrorMessage] = useState(null);
+  const [isLoading, setisLoading] = useState(false);
+  const navigate = useNavigate();
+  const eventHnadler = (e) => {
+    setformData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+  const handleSubmitted = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      return seterrorMessage("Please fill out all fields...");
+    }
+
+    console.log("majid chor");
+
+    try {
+      setisLoading(true);
+      seterrorMessage(null);
+      const res = await fetch("/api/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        seterrorMessage(data.message);
+        setisLoading(false);
+      }
+      setisLoading(false);
+      if (res.ok) {
+        navigate("/sign-in");
+      }
+    } catch (err) {
+      seterrorMessage(err.message);
+      setisLoading(false);
+    }
+  };
+  console.log("iam form", formData);
   return (
     <div className="min-h-screen mt-20">
       <div className=" flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
@@ -21,21 +59,47 @@ export default function SignUp() {
         </div>
         {/* right side */}
         <div className="flex-1">
-          <form className="flex flex-col gap-4">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmitted}>
             <div className="">
               <Label value="Your Username" />
-              <TextInput type="text" placeholder="Username" id="username" />
+              <TextInput
+                type="text"
+                placeholder="Username"
+                id="username"
+                onChange={eventHnadler}
+              />
             </div>
             <div className="">
               <Label value="Your Email" />
-              <TextInput type="email" placeholder="Email" id="email" />
+              <TextInput
+                type="email"
+                placeholder="Email"
+                id="email"
+                onChange={eventHnadler}
+              />
             </div>
             <div className="">
               <Label value="Your Password" />
-              <TextInput type="password" placeholder="password" id="password" />
+              <TextInput
+                type="password"
+                placeholder="password"
+                id="password"
+                onChange={eventHnadler}
+              />
             </div>
-            <Button gradientDuoTone="purpleToPink" type="submit">
-              Sign Up
+            <Button
+              gradientDuoTone="purpleToPink"
+              type="submit"
+              disabled={isLoading}
+            >
+              {isLoading ? (
+                <>
+                  <Spinner size="sm" />
+                  <span className="pl-3">Loading....</span>
+                </>
+              ) : (
+                "Sign Up"
+              )}
             </Button>
           </form>
           <div className="flex gap-2 text-sm mt-5">
@@ -44,6 +108,11 @@ export default function SignUp() {
               Sign In
             </Link>
           </div>
+          {errorMessage && (
+            <Alert className="mt-5" color="failure">
+              {errorMessage}
+            </Alert>
+          )}
         </div>
       </div>
     </div>
