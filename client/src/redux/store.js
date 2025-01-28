b7fa2d7ed18fb10093d360e0c502d87c9bd259bf
Chumar <chumarsarwar3@gmail.com>
2025-01-28 15:19:33 +0500

    Add persister of reacttoolkit

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,26 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import userReducer from "./reduxslice/userSlice";
 import themeReducer from "./theme/themeSlice";
-export const store = configureStore({
-  reducer: {
-    user: userReducer,
-    theme: themeReducer,
-  },
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import persistStore from "redux-persist/es/persistStore";
+const rootReducer = combineReducers({
+  user: userReducer,
+  theme: themeReducer,
 });
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+export const persistor = persistStore(store);
